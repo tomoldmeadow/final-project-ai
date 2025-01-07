7c9b11dada18b677874d344be084ff1f642db8fe
tomoldmeadow <tomoldmeadow@icloud.com>
2025-01-07 09:00:35 -0500

    updated function

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,4 +7,28 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=header)
 
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    # Extracting required set of emotions
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    #Determining the dominant emotion
+    scores = (anger_score, disgust_score, fear_score, joy_score, sadness_score)
+    labels = ('anger', 'disgust', 'fear', 'joy', 'sadness')
+
+    high_score = max(scores)
+    dominant_emotion = labels[scores.index(high_score)]
+
+    #Output as required
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+        }
